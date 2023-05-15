881eccc8f8bc6324b8d1721f58c3906b428fa118
aazam123 <113224813+aazam123@users.noreply.github.com>
2023-05-15 01:26:01 -0700

    Add files via upload

---
+++ Aazam_1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<a href="index.php"><img src="img/logo.png"></a>
+		<nav>
+			<ul>
+				<li><a>Home</a></li>
+				<li><a>About Us</a></li>
+				<li><a>Services</a></li>
+				<li><a>Portfolio</a></li>
+				<li><a>Blog</a></li>
+				<li><a>Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section>
+		<img src="">
+	</section>
+
+</body>
+</html>
